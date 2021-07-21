d17f644c5e2d089ef3cbcb619d6d17531f06f5fd
时之虫 <62002934+ToKiNoBug@users.noreply.github.com>
2021-07-21 22:53:08 +0800

    初次训练

--- RgbRot.m
+++ RgbRot.m
@@ -6,7 +6,7 @@ b1=b1+1e-10;
 r2=r2+1e-10;
 g2=g2+1e-10;
 b2=b2+1e-10;
-L=max(max(size(r1)),max(size(r2)));
+% L=max(max(size(r1)),max(size(r2)));
 sigmaRGB=(r1+r2+g1+g2+b1+b2)/3;
 S_r=min((r1+r2)./sigmaRGB,1);
 S_g=min((g1+g2)./sigmaRGB,1);
@@ -14,7 +14,7 @@ S_b=min((b1+b2)./sigmaRGB,1);
 
 theta=2/pi*acos((r1.*r2+g1.*g2+b1.*b2)./sqrt((r1.*r1+g1.*g1+b1.*b1).*(r2.*r2+g2.*g2+b2.*b2)));
 
-temp=abs(r1-r2)./(r1+r2)+abs(g1-g2)./(g1+g2)+abs(b1-b2)./(b1+b2);
+temp=abs(r1-r2)./(r1+r2)+abs(g1-g2)./(g1+g2)+abs(b1-b2)./(b1+b2)+1e-10;
 
 Str=abs(r1-r2)./(r1+r2)./temp.*S_r.^2;
 Stg=abs(g1-g2)./(g1+g2)./temp.*S_g.^2;
