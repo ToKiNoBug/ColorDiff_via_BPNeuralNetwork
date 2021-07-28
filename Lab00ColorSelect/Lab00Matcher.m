c4517bef07a124462e1a19b29f480e9ed74b00bd
时之虫 <62002934+ToKiNoBug@users.noreply.github.com>
2021-07-28 10:49:54 +0800

    准备训练数据

--- Lab00ColorSelect/Lab00Matcher.m
+++ Lab00ColorSelect/Lab00Matcher.m
@@ -1,6 +1,6 @@
 function [Diff] = Lab00Matcher(Color1,Color2)
-%size(Color1)=[3,1] , size(Color2)=[3,N] , size(Diff)=[N,1]
+%size(Color1)=[1,3] , size(Color2)=[N,3] , size(Diff)=[N,1]
 
-N=size(Color2,2);
-
+N=size(Color2,1);
+Diff=ColorDiffLab00(repmat(Color1(1),[N,1]),repmat(Color1(2),[N,1]),repmat(Color1(3),[N,1]),Color2(:,1),Color2(:,2),Color2(:,3));
 end
